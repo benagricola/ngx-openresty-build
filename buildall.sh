04bad8e604b3a79579dcce263780c6be41c22e35
Adrian Muraru <amuraru@adobe.com>
2014-08-09 00:48:15 +0300

    Build DEB package

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "====== Bootstrapping, building and packaging all OS packages ======"
-#./build-env/bin/fab bootstrap:sl65_64,trusty_64 config_openresty prereqs_openresty build_openresty package_openresty build_clean
-./build-env/bin/fab bootstrap:sl65_64 config_openresty prereqs_openresty build_openresty package_openresty build_clean
+./build-env/bin/fab bootstrap:sl65_64,trusty_64 config_openresty prereqs_openresty build_openresty package_openresty build_clean
+#./build-env/bin/fab bootstrap:sl65_64 config_openresty prereqs_openresty build_openresty package_openresty build_clean
 echo "#=================================================================#"
 echo "#                                                                 #"
 echo "#          CHECK build-output for exported OS packages!           #"
