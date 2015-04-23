a82bd20d99f0cb2565df56809c5bbe9a74b7c351
Adrian Muraru <amuraru@adobe.com>
2015-04-23 14:40:02 +0300

    Fabric fails to run parallel builds

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "====== Bootstrapping, building and packaging all OS packages ======"
-./build-env/bin/fab bootstrap:sl65_64,ct7_64 config_openresty prereqs_openresty build_openresty package_openresty build_clean
+./build-env/bin/fab bootstrap:ct7_64 config_openresty prereqs_openresty build_openresty package_openresty build_clean &
+./build-env/bin/fab bootstrap:sl65_64 config_openresty prereqs_openresty build_openresty package_openresty build_clean &
+wait
+
 #./build-env/bin/fab bootstrap:sl65_64 config_openresty prereqs_openresty build_openresty package_openresty build_clean
 echo "#=================================================================#"
 echo "#                                                                 #"
